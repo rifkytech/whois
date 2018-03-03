b8d68cf0ff8fb4e692fe8af05cd01633370befa5
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-03-03 19:50:23 +0700

    Add Font Awesome CDN

--- component/header.php
+++ component/header.php
@@ -18,8 +18,8 @@
   <title>Whois Data Domain & IP  <?php echo "$domain"; ?></title>
 
   <!-- Styles -->
-
-  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link href="assets/css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link href="assets/css/styles.css" rel="stylesheet">
 </head>
