49800c58d1c5115e03c8f4f9a2032f03043e03f3
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-03-03 19:48:29 +0700

    Update Styless

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <div class="site-wrapper-inner">
       <div class="main-container">
         <div class="inner cover">
-          <span class="glyphicon glyphicon-link"></span>
+          <h2><i class="fa fa-globe"></i></h2>
             <h1>Whois Data Domain & IP</h1>
-              <h4>whois.mexio.us</h4>
+              <h4>Free Whois Service For All</h4>
               <div class="row">
             <div class="col-lg-12">
           <form action="<?=$_SERVER['PHP_SELF'];?>">
