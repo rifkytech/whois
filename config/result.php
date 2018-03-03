99feb00fc77cd936e39c7ee0613514167fc32f67
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-03-03 08:23:27 +0700

    Create result.php

---
+++ config/result.php
@@ -0,0 +1,15 @@
+<?
+if($domain) {
+  $domain = trim($domain);
+  if(substr(strtolower($domain), 0, 7) == "http://") $domain = substr($domain, 7);
+  if(substr(strtolower($domain), 0, 4) == "www.") $domain = substr($domain, 4);
+  if(ValidateIP($domain)) {
+    $result = LookupIP($domain);
+  }
+  elseif(ValidateDomain($domain)) {
+    $result = LookupDomain($domain);
+  }
+  else die("Invalid Input!");
+  echo "<pre>\n" . $result . "\n</pre>\n";
+}
+?>
