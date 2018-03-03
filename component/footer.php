45ad0863d6c6fdbe255723ec17e1a9b3caa4ef47
Muhammad Rifky Abimayu <34646726+rifkytech@users.noreply.github.com>
2018-03-03 08:24:34 +0700

    Create footer.php

---
+++ component/footer.php
@@ -0,0 +1,15 @@
+<br>
+                <br>
+                     <p>Copyright © 2018 Mexious Media. Version : v3.0</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!-- JavaScript -->
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/custom.min.js"></script>
+
+</body>
+</html>
